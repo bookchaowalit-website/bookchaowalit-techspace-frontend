98f59eb06137eef7d0972efd027bb4bfaa738c8b
bookchaowalit <bookchaowalit@gmail.com>
2025-12-31 22:29:18 +0700

    chore: make PostCSS config compatible with Next.js (use plugin names)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
-import tailwindcss from 'tailwindcss';
-
-const config = {
-  plugins: [tailwindcss],
+export default {
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+  },
 };
-
-export default config;
